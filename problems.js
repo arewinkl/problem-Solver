a07b0c2f761835bd976a141fa8fd1ef2e69df934
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-21 17:52:37 -0600

    4-21-22

--- problems.js
+++ problems.js
@@ -1,3 +1,4 @@
+//April-21-2022
 //   Task
 // N lamps are placed in a line, some are switched on and some are off. What is the smallest number of lamps that need to be switched so that on and off lamps will alternate with each other?
 // You are given an array a of zeros and ones - 1 mean switched-on lamp and 0 means switched-off.
