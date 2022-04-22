8930e9930efa2d8926c22603ff18bd636e57994a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-21 18:16:02 -0600

    4-21-22

--- problems.js
+++ problems.js
@@ -44,3 +44,11 @@ var humanYearsCatYearsDogYears = function (humanYears) {
 
   return newArray;
 };
+
+// Write a function that returns a string in which firstname is swapped with last name.
+// nameShuffler('john McClane'); => "McClane john"
+function nameShuffler(str) {
+  const newStr = str.split(" ").reverse().join(" ");
+
+  return newStr;
+}
