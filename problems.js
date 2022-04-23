043ce089dd931a8d8c67ce623e48597f87d35f4d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-22 19:12:50 -0600

    4-22-22

--- problems.js
+++ problems.js
@@ -70,3 +70,20 @@ function isNarcissistic(n) {
     return false;
   }
 }
+
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+function stringClean(s) {
+  const string = s.split("");
+  const newArray = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (/\d/.test(string[i]) == false) {
+      newArray.push(string[i]);
+    }
+  }
+  return newArray.join("");
+}
