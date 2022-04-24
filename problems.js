e10d488b53d029b1f23f4484881fd60b4d87633c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-24 17:09:18 -0600

    4-24-22

--- problems.js
+++ problems.js
@@ -288,6 +288,12 @@ function stringy(size) {
 //? The example below is a second way to solve this problem. it is deconstructed since it takes in only one parameter. What it does is repeats the string "10" the number of times size, and then it slices off all other numbers except for the first three. it starts at index [0] and then keeps the specific number of elements passed in which was size (3).
 const stringy = (size) => "10".repeat(size).slice(0, size);
 
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// Examples:
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
 const rps = (p1, p2) => {
   if (p1 == "rock" && p2 == "scissors") {
     return "Player 1 won!";
