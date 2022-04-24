f3364ee694bffc6c85ba1c02c3a22d1922f10ee4
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-23 18:47:52 -0600

    4-23-22

--- problems.js
+++ problems.js
@@ -230,3 +230,13 @@ function barista(coffees) {
     });
   }
 }
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+function capitalizeWord(word) {
+  const firstLetter = word[0].toUpperCase();
+  const length = word.length;
+  const part = word.split("").splice(1, length).join("");
+  return firstLetter + part;
+}
