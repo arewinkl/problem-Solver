109fb8146058eb79991bb7f14388659f6751e3fb
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-25 16:04:09 -0600

    4-25-22

--- problems.js
+++ problems.js
@@ -377,6 +377,10 @@ function dropCap(str) {
     .join(" ");
 }
 
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about that!
 function removeEveryOther(arr) {
   const newArray = [];
   for (let x = 0; x < arr.length; x++) {
