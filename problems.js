10a412ed65257d5d370e2b2285685032e9d4119e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-27 15:57:51 -0600

    4-27-22

--- problems.js
+++ problems.js
@@ -456,6 +456,7 @@ function chain(input, fs) {
   }, input);
 }
 
+// Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() function of the built-in javascript Array object. If you're not familiar with reduce(), reading over the documentation may help.
 function sum(array) {
   return array.reduce((a, b) => {
     return a + b;
