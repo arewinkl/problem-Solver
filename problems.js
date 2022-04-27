7f7437cabfcadb1cb49ea27593cc33e4f952a623
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-27 17:07:35 -0600

    4-27-22

--- problems.js
+++ problems.js
@@ -477,3 +477,17 @@ function bestPrice(array) {
     }) / array.length
   );
 }
+
+function zeroAndOne(s) {
+  console.log(s);
+  let out = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1] || i === s.length - 1) {
+      out++;
+    } else {
+      i++;
+    }
+  }
+  //coding and coding..
+  return out;
+}
