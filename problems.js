1cd156adcfc52c9ea830a7716b0ac59b91d966ed
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-27 16:13:08 -0600

    4-27-22

--- problems.js
+++ problems.js
@@ -463,6 +463,11 @@ function sum(array) {
   });
 }
 
+// You must use the .reduce() method within your answer
+// Three sisters run Goodness Gracious Green (a shop selling all things green and lovely).
+// Since opening, they've struggled to agree on the sale cost of products. One believes costs should be kept as low as possible, the middle sister wants to keep an average price and the eldest thinks that they should charge more for their one-of-a-kind green things.
+// Your task is to create a function called bestPrice which will take an array of 3 prices which should be averaged to return the best and final price.
+// With your help the sisters can put an end to their fights once and for all!
 function bestPrice(array) {
   //enter code here
   console.log(array);
