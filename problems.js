f1f83d64c0dc233b666db1c5b1e48fdd8e327ee2
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-26 18:14:57 -0600

    4-26'22

--- problems.js
+++ problems.js
@@ -418,6 +418,10 @@ function multiply(a, b) {
   // Write here your multiply-function
 }
 
+// Write a function to split a string and convert it into an array of words.
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 function stringToArray(string) {
   return string.split(" ");
 }
