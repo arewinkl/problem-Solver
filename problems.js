f1fa50a7eb2f27cc294d4b5aa4a9dc11055eb5e2
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-27 15:33:10 -0600

    4-27-22

--- problems.js
+++ problems.js
@@ -438,6 +438,9 @@ function squareAreaToCircle(size) {
   return circle;
 }
 
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for ruby).
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as it's input). Return the final value after execution is complete.
 function chain(input, fs) {
   return fs.reduce(function (a, b) {
     console.log(b(a));
