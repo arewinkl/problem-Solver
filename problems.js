e4d24a5962d1d650edd043ac672411a49fa6a8a0
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-29 17:32:44 -0600

    4-29-22

--- problems.js
+++ problems.js
@@ -549,3 +549,30 @@ function plural(n) {
 function plural(n) {
   return n === 1 ? false : true;
 }
+
+function countPositivesSumNegatives(input) {
+  const newArray = [];
+  let positive = 0;
+  if (input == null || input == "") {
+    return newArray;
+  } else {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] > 0) {
+        positive++;
+      }
+    }
+    newArray.push(positive);
+    newArray.push(
+      input
+        .filter((num) => {
+          if (num < 0) {
+            return num;
+          }
+        })
+        .reduce(function (a, b) {
+          return a + b;
+        }, 0)
+    );
+    return newArray;
+  }
+}
