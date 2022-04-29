cfd4ea73542fcbd090b5bc2ad0707e3c2240e8da
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-29 15:59:21 -0600

    4-29-22

--- problems.js
+++ problems.js
@@ -514,3 +514,18 @@ const ownedCatAndDog = (catYears, dogYears) => [
 // You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 // You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 // Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+function mergeArrays(arr1, arr2) {
+  const newArray = arr1.concat(arr2);
+  const newestArray = [];
+
+  newArray.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = 0; i < newArray.length; i++) {
+    if (!newestArray.includes(newArray[i])) {
+      newestArray.push(newArray[i]);
+    }
+  }
+
+  return newestArray;
+}
