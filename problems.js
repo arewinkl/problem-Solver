c8d2b6683bed5d195ebebe712d1b40e5e12ecc5f
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-29 18:57:54 -0600

    4-29-22

--- problems.js
+++ problems.js
@@ -597,6 +597,11 @@ function countPositivesSumNegatives(input) {
   return [positive, negative];
 }
 
+// Odd bits are getting ready for Bits Battles.
+// Therefore the n bits march from right to left along an 8 bits path. Once the most-significant bit reaches the left their march is done. Each step will be saved as an array of 8 integers.
+// Return an array of all the steps.
+// 1 <= n <= 8
+// NOTE: n != 0, because n represents the number of 1s.
 function bitMarch(n) {
   console.log("n", n);
   var arr = [];
