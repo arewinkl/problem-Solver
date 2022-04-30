8aee5b4a4fd812adce74e5f3afc8c9974e919d87
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-29 18:55:37 -0600

    4-29-22

--- problems.js
+++ problems.js
@@ -596,3 +596,16 @@ function countPositivesSumNegatives(input) {
 
   return [positive, negative];
 }
+
+function bitMarch(n) {
+  console.log("n", n);
+  var arr = [];
+  for (var i = 7; i >= n - 1; i--) {
+    var result = [0, 0, 0, 0, 0, 0, 0, 0];
+    for (var j = 0; j < n; j++) {
+      result[i - j] = 1;
+    }
+    arr.push(result);
+  }
+  return arr;
+}
