9ba798cad4918d8eed5126e590f66d5c1e2aa394
Alec Rewinkel <alecrewinkel@gmail.com>
2022-04-30 22:27:53 -0600

    4-30-22

--- problems.js
+++ problems.js
@@ -623,6 +623,9 @@ function binToDec(bin) {
   return parseInt(bin, 2);
 }
 
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
 function maps(x) {
   const newArray = [];
   x.map(function (x) {
