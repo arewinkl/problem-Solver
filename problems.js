c0d862a809e2f82189160e556d1e5c8d260ecf70
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-01 15:15:14 -0600

    5-1-22

--- problems.js
+++ problems.js
@@ -674,3 +674,25 @@ function maskify(cc) {
 // ("coding", "anywhere", "n")   ==>  "codinywhere"
 // ("jason", "samson", "s")      ==>  "jasamson"
 // ("wonderful", "people", "e")  ==>  "wondeople"
+
+function stringMerge(string1, string2, letter) {
+  const firstString = string1.split("");
+  const secondString = string2.split("");
+  let first = "";
+
+  for (let i = 0; i < firstString.length; i++) {
+    if (firstString[i] != letter) {
+      first += firstString[i];
+    } else {
+      break;
+    }
+  }
+  for (let x = 0; x < secondString.length; x++) {
+    if (secondString[x] == letter) {
+      const answer = secondString.splice(0, x);
+      break;
+    }
+  }
+  const newString = secondString.join("");
+  return first + newString;
+}
