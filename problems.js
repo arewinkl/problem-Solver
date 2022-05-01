f1d55c0551fd7a206b84b1ed3234f6e1e86f42a0
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-01 15:43:22 -0600

    5-1-22

--- problems.js
+++ problems.js
@@ -704,6 +704,7 @@ function stringMerge(string1, string2, letter) {
   );
 }
 
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
 class Kata {
   static getVolumeOfCuboid(length, width, height) {
     return length * width * height;
