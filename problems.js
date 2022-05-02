776ac255e068ebe6300ead88079220906fe9851e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-01 21:00:22 -0600

    5-1-22

--- problems.js
+++ problems.js
@@ -751,3 +751,16 @@ firstNonConsecutive = (a) =>
 // Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included)
 // Example
 // Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+function pipeFix(numbers) {
+  let string = numbers.length;
+  const max = numbers[string - 1];
+  const min = numbers[0];
+  const totalLength = max - min;
+  const newArray = [];
+  for (let i = 0; i <= totalLength; i++) {
+    if (numbers[0] + i <= max) {
+      newArray.push(numbers[0] + i);
+    }
+  }
+  return newArray;
+}
