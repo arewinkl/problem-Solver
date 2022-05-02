ff0e41e37d437980018f7e5068d922e129498594
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-02 17:33:56 -0600

    5-2-22

--- problems.js
+++ problems.js
@@ -807,7 +807,7 @@ function bingo(a) {
     }
   }
 }
-
+//? This second solutions uses the .every() method that checks to see if it contains every value is included in the array by using the .includes()
 function bingo(a) {
   return [2, 9, 14, 7, 15].every((x) => a.includes(x)) ? "WIN" : "LOSE";
 }
