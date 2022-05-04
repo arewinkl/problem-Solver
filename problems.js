ec3c3e807a1b60a0ae686593d2cb3b44989aa1f1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-03 21:28:43 -0600

    5-3-22

--- problems.js
+++ problems.js
@@ -846,6 +846,9 @@ function usdcny(usd) {
 //?Here is another solution to the problem that is more streamlined.
 usdcny = ($) => `${($ * 6.75).toFixed(2)} Chinese Yuan`;
 
+// Your friend invites you out to a cool floating pontoon around 1km off the beach. Among other things, the pontoon has a huge slide that drops you out right into the ocean, a small way from a set of stairs used to climb out.
+// As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+// You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
 function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
   const sharkTravel = sharkDistance / sharkSpeed;
   const youTravel = pontoonDistance / youSpeed;
