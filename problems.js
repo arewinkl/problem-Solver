270710ac23513b7afdd0f242c595c9c341f5420e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-04 18:10:42 -0600

    5-4-22

--- problems.js
+++ problems.js
@@ -914,3 +914,15 @@ function giveChange(amount) {
   }
   return arr;
 }
+
+const bills = [100, 50, 20, 10, 5, 1];
+
+const giveChange = (amount) =>
+  bills
+    .map((value) => {
+      let result = Math.trunc(amount / value);
+      amount = amount % value;
+
+      return result;
+    })
+    .reverse();
