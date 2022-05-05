8ca7e086c49664c480f35478ef54359ac414f94c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-04 18:37:41 -0600

    5-4-22

--- problems.js
+++ problems.js
@@ -941,3 +941,12 @@ function shortcut(string) {
     )
     .join("");
 }
+
+function shortcut(str) {
+  return str
+    .split("")
+    .filter(function (e) {
+      return ["a", "e", "i", "o", "u"].indexOf(e) == -1;
+    })
+    .join("");
+}
