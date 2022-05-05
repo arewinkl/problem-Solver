308dd9f2e3085179a083e1396b732625d9b0f158
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-04 18:07:23 -0600

    5-4-22

--- problems.js
+++ problems.js
@@ -878,6 +878,14 @@ function powersOfTwo(n) {
   return newArray;
 }
 
+// Create a function that will take any amount of money and break it down to the smallest number of bills as possible. Only integer amounts will be input, NO floats. This function should output a sequence, where each element in the array represents the amount of a certain bill type. The array will be set up in this manner:
+// array[0] ---> represents $1 bills
+// array[1] ---> represents $5 bills
+// array[2] ---> represents $10 bills
+// array[3] ---> represents $20 bills
+// array[4] ---> represents $50 bills
+// array[5] ---> represents $100 bills
+// In the case below, we broke up $365 into 1 $5 bill, 1 $10 bill, 1 $50 bill, and 3 $100 bills.
 function giveChange(amount) {
   var arr = [0, 0, 0, 0, 0, 0];
   while (amount >= 100) {
