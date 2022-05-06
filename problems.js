54f3ed5ddee6b57f815c5b4d72cc5958ac477de8
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-05 22:26:35 -0600

    5-5-22

--- problems.js
+++ problems.js
@@ -1015,7 +1015,7 @@ function even_or_odd(number) {
     return "Odd";
   }
 }
-
+//? This turnery simplifies the solution to this problem to a single line of code.
 function even_or_odd(number) {
   return number % 2 ? "Odd" : "Even";
 }
