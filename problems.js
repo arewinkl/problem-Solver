321f0e73b7beedcfa1d75e77e0c63bf1b60a8202
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-06 20:08:13 -0600

    5-6-22

--- problems.js
+++ problems.js
@@ -1020,6 +1020,7 @@ function even_or_odd(number) {
   return number % 2 ? "Odd" : "Even";
 }
 
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 function boolToWord(bool) {
   return bool === true ? "Yes" : "No";
 }
