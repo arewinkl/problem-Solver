fbbfe7881db0884c02c2c8236aa0a502ac40f907
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-08 16:47:58 -0600

    5-8-22

--- problems.js
+++ problems.js
@@ -1152,6 +1152,7 @@ String.prototype.toJadenCase = function () {
 // This is a simple exercise to understand the feature in the javascript language called closure.
 // The function buildFun will return an array of functions. The single parameter accepted by buildFun is the number of elements N of the array returned.
 // The wanted outcome is that when all function in the array are executed, the number from 0 to N should be returned.
+//TODO this function uses closures which privatizes what the functions can access, but since var is not mutable you have to use let which is mutable, but with the closure still has access to that variable. Look over closures more to get a better grasp on what is going on.
 function buildFun(n) {
   var res = [];
 
