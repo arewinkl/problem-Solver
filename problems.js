15ebb3609f28dc2bede7b16353b21745cb72a883
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-08 12:50:59 -0600

    5-8-22

--- problems.js
+++ problems.js
@@ -1115,6 +1115,11 @@ function pillars(num_pill, dist, width) {
   }
 }
 
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+// For example:
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
 function between(a, b) {
   const newArray = [];
   for (let i = a; i <= b; i++) {
