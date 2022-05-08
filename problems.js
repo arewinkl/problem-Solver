cefe96053278a0c7f37ce547ce55615324ce8987
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-08 12:31:37 -0600

    5-8-22

--- problems.js
+++ problems.js
@@ -1093,6 +1093,11 @@ function smash(words) {
   return words.join(" ");
 }
 
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
 function pillars(num_pill, dist, width) {
   const distance = dist * 100 * (num_pill - 1);
   const pillSize = (num_pill - 2) * width;
