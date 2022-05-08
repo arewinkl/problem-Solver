9f39f76a3fafadf4925b0b08bd02cd4fbad8e267
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-08 11:39:10 -0600

    5-8-22

--- problems.js
+++ problems.js
@@ -1087,6 +1087,8 @@ function symmetricPoint(p, q) {
   return p.map((a, i) => q[i] * 2 - a);
 }
 
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 function smash(words) {
   return words.join(" ");
 }
