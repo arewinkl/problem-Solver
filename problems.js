bc5187aeca2b93759a4118881f632e50d22816de
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-08 16:46:00 -0600

    5-8-22

--- problems.js
+++ problems.js
@@ -1149,6 +1149,9 @@ String.prototype.toJadenCase = function () {
   });
 };
 
+// This is a simple exercise to understand the feature in the javascript language called closure.
+// The function buildFun will return an array of functions. The single parameter accepted by buildFun is the number of elements N of the array returned.
+// The wanted outcome is that when all function in the array are executed, the number from 0 to N should be returned.
 function buildFun(n) {
   var res = [];
 
