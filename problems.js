027f2fcf2ec28284be456e650b0b5541c3117b30
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-10 15:53:36 -0600

    5-10-22

--- problems.js
+++ problems.js
@@ -1274,6 +1274,11 @@ function wrap(value) {
   return { value };
 }
 
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
 function testEven(n) {
   return n % 2 === 0;
 }
