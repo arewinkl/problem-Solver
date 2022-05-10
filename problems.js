5bd872cc725ef51f5329b4a608259a1e93e4ac96
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-10 14:47:26 -0600

    5-10-22

--- problems.js
+++ problems.js
@@ -1222,3 +1222,18 @@ function mountainsOfHoiyama(width) {
 //? this is a simple solition that uses the math to calculate the size of the mountain.
 const mountainsOfHoiyama = (width) =>
   ((width + 1) * (width ** 2 + 2 + width)) / 8;
+
+//   In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+// Your function will be tested with pre-made examples as well as random ones.
+// If you can, try writing it in one line of code.
+function findDifference(a, b) {
+  const first = a[0] * a[1] * a[2];
+  const second = b[0] * b[1] * b[2];
+
+  if (first > second) {
+    return first - second;
+  } else {
+    return second - first;
+  }
+}
