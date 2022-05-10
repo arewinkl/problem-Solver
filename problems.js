694c40381289c02bae02e16449ab42f6b3e3a14e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-09 22:43:57 -0600

    5-9-22

--- problems.js
+++ problems.js
@@ -1183,6 +1183,32 @@ function counter() {
   };
 }
 
+// n the planet named Hoiyama, scientists are trying to find the weights of the mountains.
+// They managed to find the weights of some mountains.
+// But calculating them manually takes a really long time.
+// That's why they hired you to develop an algorithm and easily calculate the weights of the mountains.
+// Your function has only one parameter which is the width of the mountain and you need to return the weight of that mountain.
+// The widths of the mountains are only odd numbers.
+// They gave you some information about calculating the weight of a mountain.
+// Examine the information given below and develop a solution accordingly.
+// width = 3
+//           3          ->  3
+//         1,2,1        ->  4
+//                       +___
+//                weight:   7
+// width = 5
+//           5          ->  5
+//         3,4,3        -> 10
+//       1,2,3,2,1      ->  9
+//                       +___
+//                weight:  24
+// width = 7
+//           7          ->  7
+//         5,6,5        -> 16
+//       3,4,5,4,3      -> 19
+//     1,2,3,4,3,2,1    -> 16
+//                       +___
+//                weight:  58
 function mountainsOfHoiyama(width) {
   let result = 0;
   for (let i = 1; i <= (width - 1) / 2; i++) {
