70277c163ced22a280dbaf625baab372fb47d387
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-11 18:40:39 -0600

    5-11-22

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -7,6 +7,13 @@ function warnTheSheep(queue) {
     : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
 }
 
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+// Example: (Input1, Input2 -->Output)
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
 function sumStr(a, b) {
   const answer = Number(a) + Number(b);
   return answer.toString();
