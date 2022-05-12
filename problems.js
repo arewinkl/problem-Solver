feebdca2241a68ce92bdde2c79ea34dea0bc0089
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-11 18:24:29 -0600

    5-11-22

--- problems.js
+++ problems.js
@@ -1286,10 +1286,15 @@ function testEven(n) {
 // Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
 // Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
 function warnTheSheep(queue) {
+  console.log(queue);
+  console.log(queue.reverse());
+  console.log(queue.length);
   for (let i = 0; i < queue.length; i++) {
+    console.log(i);
     if (queue[0] == "wolf") {
       return "Pls go away and stop eating my sheep";
     } else if (queue[i] !== "sheep") {
+      console.log("bahhhh", [i]);
       return `Oi! Sheep number ${i}! You are about to be eaten by a wolf!`;
     }
   }
