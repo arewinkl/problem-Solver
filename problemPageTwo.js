0ef00ffdc54687a31aa6d2371ebe404afd284ec2
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-12 21:53:11 -0600

    5-12-22

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -111,3 +111,9 @@ function points(games) {
     return a + b;
   });
 }
+
+const points = (games) =>
+  games.reduce((output, current) => {
+    return (output +=
+      current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0);
+  }, 0);
