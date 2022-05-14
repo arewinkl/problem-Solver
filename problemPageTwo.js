c8849ed9b8c58e3921bf55843602844ad7bc3f36
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-14 14:04:14 -0600

    weekend code

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -134,3 +134,10 @@ function howMuchWater(water, load, clothes) {
     return Number(answer);
   }
 }
+
+const howMuchWater = (water, load, clothes) =>
+  clothes > 2 * load
+    ? "Too much clothes"
+    : clothes < load
+    ? "Not enough clothes"
+    : Math.round(water * Math.pow(1.1, clothes - load) * 100) / 100;
