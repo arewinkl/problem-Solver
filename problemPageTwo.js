af2e472eae36ffc4815514fd3aeeadce75202936
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-15 15:08:04 -0600

    weekend code

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -175,3 +175,11 @@ hello = (n) =>
           .replace(/(?<=.).+/, Function.call.bind(String.prototype.toLowerCase))
       : "World"
   }!`;
+
+function reverseList(list) {
+  const newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+}
