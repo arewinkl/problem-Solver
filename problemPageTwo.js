2d0a44227cdaf31d259f464164301e81616fd91c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-15 18:09:53 -0600

    weekend code

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -230,6 +230,14 @@ function doubleChar(str) {
     .join("");
 }
 
+// Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+// Return n if the input is already a multiple of three, and if no such number exists, return null, a similar empty value, or -1.
+// Examples
+// 1      => null
+// 25     => null
+// 36     => 36
+// 1244   => 12
+// 952406 => 9
 const prevMultOfThree = (n) => {
   const length = n.toString().split("").length;
   if (length == 1 && n % 3 !== 0) {
