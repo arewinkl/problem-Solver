0877b59aa971c3700e784d3071ac7799fa45af6f
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-15 18:17:54 -0600

    weekend code

--- problemPageTwo.js
+++ problemPageTwo.js
@@ -254,3 +254,9 @@ const prevMultOfThree = (n) => {
     }
   }
 };
+
+const prevMultOfThree = (n) => {
+  if (n === 0) return null;
+  if (n % 3 === 0) return n;
+  return prevMultOfThree(+n.toString().slice(0, -1));
+};
