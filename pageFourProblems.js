cee5b6d142912704d93dbd32b31943c030b2643f
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-21 12:33:53 -0600

    5-21-22

--- pageFourProblems.js
+++ pageFourProblems.js
@@ -27,3 +27,15 @@ function addExtra(listOfNumbers) {
 function take(arr, n) {
   return arr.slice(0, n);
 }
+
+function take(arr, n) {
+  let counter = 0;
+  let res = [];
+  for (let i of arr) {
+    counter += 1;
+    if (counter <= n && counter <= arr.length) {
+      res.push(i);
+    }
+  }
+  return res;
+}
