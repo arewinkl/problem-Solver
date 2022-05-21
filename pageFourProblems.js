ce133bd2b8914d80d103d0b7324bd0e4dd704535
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-21 16:11:25 -0600

    5-21-22

--- pageFourProblems.js
+++ pageFourProblems.js
@@ -41,6 +41,7 @@ function take(arr, n) {
   return res;
 }
 
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 function isDivisible(n, x, y) {
   console.log(n, x, y);
   return n % x == 0 && n % y == 0 ? true : false;
