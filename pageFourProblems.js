c12dffc813f38eca9a59e51ba964c7fe388e2c21
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-22 15:22:34 -0600

    sunday coding

--- pageFourProblems.js
+++ pageFourProblems.js
@@ -51,6 +51,12 @@ function isDivisible(n, x, y) {
   return !(n % x || n % y);
 }
 
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+// For example:
+// [2, 1, 10]  ->  9
+// In descending order: [10, 2, 1]
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+// If the array is empty or the array has only one element the result should be 0 ( Nothing in Haskell ).
 function sumOfDifferences(arr) {
   const play = arr.sort((a, b) => {
     return b - a;
