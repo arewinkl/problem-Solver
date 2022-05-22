32712e76321f11f000aab2cf0ae4ab9b8b25a66d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-22 17:55:47 -0600

    sunday coding

--- pageFourProblems.js
+++ pageFourProblems.js
@@ -96,6 +96,16 @@ function remove(s) {
   return s.endsWith("!") ? s.slice(0, -1) : s;
 }
 
+// Your Job
+// Find the sum of all multiples of n below m
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
 function sumMul(n, m) {
   let sum = 0;
 
