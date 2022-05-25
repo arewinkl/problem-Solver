c2c65150d453bae0eb68a753abf3aedee8c7fda1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-24 21:18:25 -0600

    code practice

--- pageFiveProblems.js
+++ pageFiveProblems.js
@@ -52,3 +52,13 @@ function finalGrade(exam, projects) {
     return 0;
   }
 }
+
+const finalGrade = (exam, projects) => {
+  return exam > 90 || projects > 10
+    ? 100
+    : exam > 75 && projects >= 5
+    ? 90
+    : exam > 50 && projects >= 2
+    ? 75
+    : 0;
+};
