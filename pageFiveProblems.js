e3621ae1843b589489af035d6195ac320d87540d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-05-31 16:59:46 -0600

    sunday prob

--- pageFiveProblems.js
+++ pageFiveProblems.js
@@ -79,6 +79,8 @@ function billboard(name, price = 30) {
   return name.split("").reduce((sum, letter) => sum + price, 0);
 }
 
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 const quarterOf = (month) => {
   return Math.ceil((month / 12) * 4);
 };
