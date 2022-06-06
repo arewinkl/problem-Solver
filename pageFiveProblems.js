b3e78b4c127508e38423b9c05299f77a19c4df1e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-05 20:34:00 -0600

    sunday code

--- pageFiveProblems.js
+++ pageFiveProblems.js
@@ -113,3 +113,14 @@ function makeUpperCase(str) {
 // "ACSKLDFJSgSKLDFJSKLDFJ" -> False
 // "ACSKLDFJSGSKLDFJSKLDFJ" -> True
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function () {
+  let sum = 0;
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == this[i].match(/[a-z]/)) {
+      console.log("yep");
+      sum++;
+    }
+  }
+  return sum > 0 ? false : true;
+};
