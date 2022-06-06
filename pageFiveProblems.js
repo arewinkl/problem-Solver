08c83bc97e5de60806e4c5f87a720f990a9ecb1b
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-05 20:41:14 -0600

    sunday coding

--- pageFiveProblems.js
+++ pageFiveProblems.js
@@ -129,3 +129,11 @@ String.prototype.isUpperCase = function () {
 String.prototype.isUpperCase = function () {
   return !/[a-z]/.test(this);
 };
+
+String.prototype.isUpperCase = function () {
+  if (this.toString() == this.toUpperCase()) {
+    return true;
+  } else {
+    return false;
+  }
+};
