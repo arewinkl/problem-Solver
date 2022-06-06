006ae6eaf5e3ca9fe974770ee21e130f24851318
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-05 20:54:37 -0600

    coding

--- pageFiveProblems.js
+++ pageFiveProblems.js
@@ -139,6 +139,8 @@ String.prototype.isUpperCase = function () {
   }
 };
 
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+// Note: input will never be an empty string
 function fakeBin(x) {
   const newArray = [];
   const nums = x.split("");
