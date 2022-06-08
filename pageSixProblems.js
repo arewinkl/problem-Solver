e1c40c069e08315512183a60fb3ceeded24784a1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-07 19:14:42 -0600

    coding

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -1,2 +1,9 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+function squareSum(numbers) {
+  console.log(numbers);
+  let sum = 0;
+  numbers.map((n) => ((sum += n ** 2), console.log(n ** 2)));
+  console.log(sum);
+  return sum;
+}
