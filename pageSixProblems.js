d3d4965c347639953369ad3bd9e199ac2201a80c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-11 12:04:38 -0600

    coding

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -13,3 +13,11 @@ function squareSum(numbers) {
     return n * n + sum;
   }, 0);
 }
+
+function stairsIn20(s) {
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    sum += s[i].reduce((a, b) => a + b);
+  }
+  return sum * 20;
+}
