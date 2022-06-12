171ed9dc9ca20ee421b441b1b6a861f2a59a5ae1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-11 23:14:23 -0600

    code

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -59,3 +59,30 @@ for (let i = 0; i < FLAG.length; i += 16) {
   console.log(FLAG.slice(i, i + 16));
   res.push(FLAG.slice(i, i + 16));
 }
+
+const get_neighbourhood = function (type, arr, [y, x]) {
+  if (!arr[y] || arr[y][x] === undefined) return [];
+  const arrType =
+    type === "moore"
+      ? [
+          [y - 1, x - 1],
+          [y - 1, x],
+          [y - 1, x + 1],
+          [y, x - 1],
+          [y, x + 1],
+          [y + 1, x - 1],
+          [y + 1, x],
+          [y + 1, x + 1],
+        ]
+      : [
+          [y, x - 1],
+          [y, x + 1],
+          [y - 1, x],
+          [y + 1, x],
+        ];
+
+  return arrType
+    .filter(([a, b]) => arr[a] && arr[a][b] !== undefined)
+    .map(([e1, e2]) => arr[e1][e2])
+    .sort((a, b) => a < b);
+};
