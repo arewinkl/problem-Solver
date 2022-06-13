e2d459a6f69232ccf29dcc982bf56d07f53d32e3
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-12 19:31:14 -0600

    code

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -128,6 +128,11 @@ function getNeighbourhood(type, matrix, coordinates) {
     ].filter((s) => s >= 0);
   }
 }
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
 
 function sayHello(name) {
   console.log(name);
