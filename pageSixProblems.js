6bd4ac8f9803f6cbcb37d6a4f31c090d80212c10
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-14 06:42:31 -0600

    code tuesday

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -177,7 +177,7 @@ function iceBrickVolume(r, h, d) {
 function remainder(n, m) {
   return n > m ? n % m : m % n;
 }
-
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 function removeChar(str) {
   return str.slice(1, -1);
 }
