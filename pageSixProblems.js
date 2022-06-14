39b6260bb65d5f464626b00e0ec75866a99d1d3c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-14 06:48:08 -0600

    code tuesday

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -182,6 +182,9 @@ function removeChar(str) {
   return str.slice(1, -1);
 }
 
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// For example, when the input is green, output should be yellow.
 function updateLight(current) {
   console.log(current);
   //your code here!
