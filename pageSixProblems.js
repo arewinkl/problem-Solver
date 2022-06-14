bb2656ce5c302c9d2281ab39d27e1d5c4c5a96e9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-14 06:49:23 -0600

    code tuesday

--- pageSixProblems.js
+++ pageSixProblems.js
@@ -196,3 +196,11 @@ function updateLight(current) {
     return "green";
   }
 }
+
+function updateLight(current) {
+  return current === "yellow"
+    ? "red"
+    : current === "green"
+    ? "yellow"
+    : "green";
+}
