4e89e95473d9e97fba71b4d5f251aa254c3ac45e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-16 19:09:51 -0600

    coding

--- pageSevenProblems.js
+++ pageSevenProblems.js
@@ -20,10 +20,13 @@ function square(a) {
   console.log(a * a);
   return a * a;
 }
-
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+// Create a function which translates a given DNA string into RNA.
+// For example:
+// "GCAT" => "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 function DNAtoRNA(dna) {
   const data = dna.split("");
-
   const newArray = [];
   for (let i = 0; i < dna.split("").length; i++) {
     if (data[i] == "T") {
