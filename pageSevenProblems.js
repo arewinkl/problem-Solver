fa6c39345f24880d469bfb0ffc48715eaf2fa39a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-21 20:30:29 -0600

    code probs

--- pageSevenProblems.js
+++ pageSevenProblems.js
@@ -124,3 +124,6 @@ function expressionMatter(a, b, c) {
 // Usage Examples:
 // cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+function enough(cap, on, wait) {
+  return on + wait <= cap ? 0 : on + wait - cap;
+}
