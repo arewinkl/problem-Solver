bef6212b1bb31101bed25f76c0f1c7987e645161
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-24 22:25:57 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -37,7 +37,7 @@ function likeOrDislike(buttons) {
   console.log(res);
   return res;
 }
-
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
 var a = "code";
 var b = "wa.rs";
 console.log(a, b);
