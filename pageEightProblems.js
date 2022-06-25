1e2599eada818ea31f6a27bbeff56ae1f9d97b05
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-24 22:46:35 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -42,7 +42,10 @@ var a = "code";
 var b = "wa.rs";
 console.log(a, b);
 var name = a + b;
-
+// Your function takes two arguments:
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   console.log(dadYearsOld, sonYearsOld);
   return Math.abs(dadYearsOld - sonYearsOld * 2);
