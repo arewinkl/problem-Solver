38792ddf1d5046cb9493b62edff77c8e74e8bea8
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-28 18:58:00 -0600

    problems

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -50,7 +50,7 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   console.log(dadYearsOld, sonYearsOld);
   return Math.abs(dadYearsOld - sonYearsOld * 2);
 }
-
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
 function buildString(...template) {
   console.log(`I like ${template.join(", ")}!`);
   return `I like ${template.join(", ")}!`;
