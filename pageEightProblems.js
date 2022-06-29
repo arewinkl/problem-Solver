025f42d1374128981af14ae05881d4bc98484e1e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-29 13:11:34 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -55,7 +55,11 @@ function buildString(...template) {
   console.log(`I like ${template.join(", ")}!`);
   return `I like ${template.join(", ")}!`;
 }
-
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
 function bmi(weight, height) {
   const bmi = weight / (height * height);
   console.log(bmi);
