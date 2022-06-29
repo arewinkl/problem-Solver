a12e901e04b0c26427d4f8b87bb771a1f2503e1a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-29 13:13:31 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -74,3 +74,15 @@ function bmi(weight, height) {
     return "Obese";
   }
 }
+
+function bmi(weight, height) {
+  var bmi = weight / (height * height);
+
+  return bmi < 18.5
+    ? "Underweight"
+    : bmi <= 25
+    ? "Normal"
+    : bmi <= 30
+    ? "Overweight"
+    : "Obese";
+}
