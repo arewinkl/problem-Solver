dc2c8e1f229e8c7bd056af9c8eae3bedef820f68
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-29 13:15:43 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -86,3 +86,12 @@ function bmi(weight, height) {
     ? "Overweight"
     : "Obese";
 }
+
+const bmi = (w, h, bmi = w / h / h) =>
+  bmi <= 18.5
+    ? "Underweight"
+    : bmi <= 25
+    ? "Normal"
+    : bmi <= 30
+    ? "Overweight"
+    : "Obese";
