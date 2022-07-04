e707406f2456b0a2942a68339a8a6b66e502831c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-04 17:59:16 -0600

    code

--- pageEightProblems.js
+++ pageEightProblems.js
@@ -152,7 +152,14 @@ function setAlarm(employed, vacation) {
 
   return (employed == true) & (vacation == false) ? true : false;
 }
-
+// Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+// a is considered "close to" b if margin is greater than or equal to the distance between a and b.
+// Please note the following:
+// When a is close to b, return 0.
+// Otherwise...
+// When a is less than b, return -1.
+// When a is greater than b, return 1.
+// If margin is not given, treat it as zero.
 function closeCompare(a, b, margin = 0) {
   if (a < b - margin) return -1;
   if (a - margin > b) return 1;
