447797b356bfa5066af3c92a6eed813bf811be13
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-08 15:34:29 -0600

    code problem

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -27,3 +27,15 @@ function brightest(colors) {
   }
   return colors[colorIndex];
 }
+
+function rentalCarCost(d) {
+  // Your solution here
+  console.log(d);
+  if (d < 3) {
+    return d * 40;
+  } else if (d < 7) {
+    return d * 40 - 20;
+  } else if (d >= 7) {
+    return d * 40 - 50;
+  }
+}
