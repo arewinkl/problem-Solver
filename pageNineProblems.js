fdc9ac3f63ef96264e1d501a82226cf38bacc4ef
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-10 18:02:59 -0600

    code

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -59,11 +59,8 @@ function amIWilson(p) {
 }
 
 // The national go-kart racing competition is taking place in your local town and you've been called for building the winners podium with the available wooden blocks. Thankfully you are in a wood-rich area, number of blocks are always at least 6.
-
 // Remember a classic racing podium have three platforms for first, second and third places. First place is the highest and second place is higher than third. Also notice that platforms are arranged as 2nd - 1st - 3rd.
-
 // The organizers want a podium that satisfies:
-
 // The first place platform has the minimum height posible
 // The second place platform has the closest height to first place
 // All platforms have heights greater than zero.
