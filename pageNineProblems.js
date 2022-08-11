cb5909d3f2028e655861a8cdd4b609f5cfcfb328
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-10 18:18:15 -0600

    problem

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -103,6 +103,11 @@ function elevator(left, right, call) {
 const elevator = (left, right, call) =>
   Math.abs(call - left) < Math.abs(call - right) ? "left" : "right";
 
+//   Area of an arrow
+//   An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+//   arrow
+//   a and b are integers and > 0
+//   Write a function which returns the area of the arrow.
 function arrowArea(a, b) {
   console.log(a, b);
   // your code
