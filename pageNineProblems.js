c6b77467dd27aec869c41b8e2938d0b2764a505e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-23 17:32:14 -0600

    code

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -142,6 +142,11 @@ function unusualFive() {
   return "catts".length;
 }
 
+// Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+// A pythagorean triple is formed when:
+// c2 = a2 + b2
+// where c is the largest value of a, b, c.
+// For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
 function isPythagoreanTriple(integers) {
   // Good luck friends!
   console.log(integers);
