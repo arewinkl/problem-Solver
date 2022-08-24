cba2d95b17f0b4218c46003e4794a8dabc3fa02c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-23 18:02:12 -0600

    new problems

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -162,3 +162,13 @@ function isPythagoreanTriple(nums) {
 
   return a ** 2 + b ** 2 === c ** 2;
 }
+
+function divisibleBy(numbers, divisor) {
+  let newArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor == 0) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
+}
