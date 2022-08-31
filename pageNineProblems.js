21287913c166c19e564ad98217a2011e26fbf276
Alec Rewinkel <alecrewinkel@gmail.com>
2022-08-30 21:17:19 -0600

    code

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -199,7 +199,9 @@ function feast(beast, dish) {
     return false;
   }
 }
-
+// he first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+// Given a year, return the century it is in.
 function century(year) {
   console.log(Math.ceil(year / 100));
   return Math.ceil(year / 100);
