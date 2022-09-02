5e16ae0b9956cc288cb8146007f594956d19e6cb
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-01 18:59:31 -0600

    code

--- pageNineProblems.js
+++ pageNineProblems.js
@@ -294,6 +294,8 @@ function sumMix(x) {
   return x.map((a) => +a).reduce((a, b) => a + b, 0);
 }
 
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 function grow(x) {
   return x.reduce((a, b) => a * b);
 }
