ee0148f608b04273c93f35f310c85a61f67aa05d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-17 19:32:04 -0600

    code kata

--- pageTenProblems.js
+++ pageTenProblems.js
@@ -154,6 +154,9 @@ function modifyMultiply(str, loc, num) {
   return (str.split(" ")[loc] + "-").repeat(num).slice(0, -1);
 }
 
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
 function squareDigits(num) {
   const newString = num.toString().split("");
   const newArray = [];
