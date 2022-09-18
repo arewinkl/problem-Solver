1350b1da198eb8139fdea3c5989b96644d1a23c8
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-17 18:51:13 -0600

    code problem

--- pageTenProblems.js
+++ pageTenProblems.js
@@ -133,6 +133,12 @@ function houseNumbersSum(inputArray) {
     else sum += inputArray[i];
 }
 
+// You are to write an function that takes a string as it's first paramter. This string will be a string of words.
+// You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+// Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word.
+// Example
+
+modifyMultiply("This is a string", 3, 5);
 function modifyMultiply(str, loc, num) {
   const newWord = str.split(" ")[loc];
   const newArray = [];
