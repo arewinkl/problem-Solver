74110bb1b0dbb4bed4ec5b1c46bceb474ea50b16
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-23 14:41:36 -0600

    code challenge

--- pageElevenProblems.js
+++ pageElevenProblems.js
@@ -68,3 +68,17 @@ function howManydays(month) {
   }
   return days;
 }
+
+function howManydays(month) {
+  switch (month) {
+    case 2:
+      return 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 31;
+  }
+}
