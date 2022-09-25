60b25b15ddb831c9bfa58f8b8594b4effda8c995
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-25 15:45:47 -0600

    instructions

--- pageElevenProblems.js
+++ pageElevenProblems.js
@@ -122,6 +122,16 @@ function getDivisorsCnt(n) {
   }
 }
 
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 var summation = function (num) {
   for (let i = 0, x = 0; i <= num; i++) {
     if (i < num) {
