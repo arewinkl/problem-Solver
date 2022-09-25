f19a7208fbb96eaddc3a75ffa3612dc657faa89c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-25 15:48:59 -0600

    code

--- pageElevenProblems.js
+++ pageElevenProblems.js
@@ -136,3 +136,11 @@ var summation = function (num) {
     } else return x + num;
   }
 };
+
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
