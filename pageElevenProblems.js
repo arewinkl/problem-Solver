95d05b2b64a25ff6d31c1e60502f9a363723fe10
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-28 20:42:20 -0600

    code kata

--- pageElevenProblems.js
+++ pageElevenProblems.js
@@ -152,6 +152,8 @@ function findAverage(array) {
   return array.length < 1 ? 0 : array.reduce((x, y) => x + y) / array.length;
 }
 
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
 function arrayPlusArray(arr1, arr2) {
   const one = arr1.reduce((x, y) => x + y);
   const two = arr2.reduce((x, y) => x + y);
