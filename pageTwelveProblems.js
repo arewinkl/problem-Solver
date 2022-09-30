2f8911ecbdb4981f1c56e8396bbca2b0e89a9841
Alec Rewinkel <alecrewinkel@gmail.com>
2022-09-29 18:07:54 -0600

    code

--- pageTwelveProblems.js
+++ pageTwelveProblems.js
@@ -91,6 +91,11 @@ function nameThatNumber(n) {
   return n ? a[n] || b[~~(n / 10)] + (n % 10 ? " " + a[n % 10] : "") : "zero";
 }
 
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
 function abbrevName(name) {
   var nameArray = name.split(" ");
   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
