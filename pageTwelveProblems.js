ddcc70508039c1f01e9fd82376fd32f0608eeaa1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-05 17:09:06 -0600

    code

--- pageTwelveProblems.js
+++ pageTwelveProblems.js
@@ -109,13 +109,13 @@ function abbrevName(name) {
     .join(".");
 }
 
-Terminal game move function
-In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
 
-Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
 
-Example:
-move(3, 6) should equal 15
+// Example:
+// move(3, 6) should equal 15
 function move(position, roll) {
   return position + roll * 2;
 }
