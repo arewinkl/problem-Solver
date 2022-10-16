97435f79cf59640d16761ab2683ba6e9c02672c0
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-16 13:14:07 -0600

    kata 6

--- pageTwelveProblems.js
+++ pageTwelveProblems.js
@@ -254,3 +254,28 @@ function strCount(str, letter) {
 function strCount(str, letter) {
   return str.split("").filter((a) => a == letter).length;
 }
+
+function isValidWalk(walk) {
+  var dx = 0;
+  var dy = 0;
+  var dt = walk.length;
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case "n":
+        dy--;
+        break;
+      case "s":
+        dy++;
+        break;
+      case "w":
+        dx--;
+        break;
+      case "e":
+        dx++;
+        break;
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0;
+}
