58f2da9c8cb3891c37fe612ac864e8a8bcf3ee77
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-16 12:46:17 -0600

    cdoe katas

--- pageTwelveProblems.js
+++ pageTwelveProblems.js
@@ -249,3 +249,7 @@ function strCount(str, letter) {
 function strCount(str, letter) {
   return str.split(letter).length - 1;
 }
+
+function strCount(str, letter) {
+  return str.split("").filter((a) => a == letter).length;
+}
