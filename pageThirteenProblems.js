6bb969d19d1eca503ca974d529b129bdd658f41d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-16 13:43:11 -0600

    difficult kata 6

--- pageThirteenProblems.js
+++ pageThirteenProblems.js
@@ -1,3 +1,35 @@
+// Task
+// Given a circuit with fixed resistors connected in series and/or in parallel, calculate the total effective resistance of the circuit.
+// All resistors are given in Ω, and the result should be in Ω too (as a float; tested to ± 1e-6). Assume wires have negligible resistance. The voltage of the battery is irrelevant.
+// The circuit is passed to your function in the following form:
+// I will define a component as any number of resistors connected in series or in parallel.
+// The entire circuit counts as a component.
+// Each component is an array.
+// A series component will have the boolean true in position zero.
+// A parallel component will have the boolean false in position zero.
+// The other positions will either contain:
+// Numbers, denoting fixed resistors of that resistance.
+// Arrays, denoting nested components.
+// A series circuit with no other entries represents a single wire
+// A parallel circuit with no other entries represents a break in the circuit (see below for more details)
+// All circuits will be valid and in the form above (short circuits or broken circuits may appear, though)
+// There will be no negative resistances
+// Example circuit:
+//   [
+//     true, // series
+//     20, // 20Ω resistor
+//     [
+//       false, // parallel
+//       [
+//         true, // series
+//         30, // 30Ω resistor
+//         40, // 40Ω resistor
+//       ],
+//       30, // 30Ω resistor
+//     ],
+//     60, // 60Ω resistor
+//   ]
+
 function calculateResistance(circuit) {
   let x = rec(circuit);
   if (x == 0) {
