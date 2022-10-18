b9c8581a891841601e4362910f5aaa08d3a6a550
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-18 07:36:16 -0600

    kata coding

--- pageThirteenProblems.js
+++ pageThirteenProblems.js
@@ -63,3 +63,16 @@ function rec(circuit) {
     );
   }
 }
+
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+  return [odd, even];
+}
