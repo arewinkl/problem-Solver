e8451a14d8f01a794a025c94c9a60dbd2a5a8794
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-18 08:22:06 -0600

    kata code

--- pageThirteenProblems.js
+++ pageThirteenProblems.js
@@ -119,6 +119,9 @@ function simpleMultiplication(number) {
   return number % 2 == 0 ? number * 8 : number * 9;
 }
 
+// Switch/Case - Bug Fixing #6
+// Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
 function evalObject(value) {
   console.log(value.a + value.b, value.operation);
   switch (value.operation) {
