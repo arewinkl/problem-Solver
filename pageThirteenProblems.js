ae81825650f399222fb58636b2ad2b47d01bacb9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-18 07:45:04 -0600

    new kata

--- pageThirteenProblems.js
+++ pageThirteenProblems.js
@@ -93,3 +93,13 @@ function pickIt(arr) {
   }
   return [odd, even];
 }
+
+function multipleOfIndex(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % i == 0) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
