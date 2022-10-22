5a99518727c548dd33e1bfff03598b74ce48d47d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-22 16:11:14 -0600

    code

--- pageFourteenProblems.js
+++ pageFourteenProblems.js
@@ -24,6 +24,14 @@ function howManyGifts(maxBudget, gifts) {
   return gifts.filter((item) => (maxBudget -= item) >= 0).length;
 }
 
+// Write a function that takes as its parameters one or more numbers which are the diameters of circles.
+// The function should return the total area of all the circles, rounded to the nearest integer in a string that says "We have this much circle: xyz".
+// You don't know how many circles you will be given, but you can assume it will be at least one.
+// So:
+// sumCircles(2) == "We have this much circle: 3"
+// sumCircles(2, 3, 4) == "We have this much circle: 23"
+// Translations and comments (and upvotes!) welcome!
+
 function sumCircles() {
   let circleSum = 0;
   let pie = Math.PI;
