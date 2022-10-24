2fe5321a935c6019f6e913c4002a04318bda99ae
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-24 16:24:16 -0600

    instructions for prototype

--- pageFourteenProblems.js
+++ pageFourteenProblems.js
@@ -77,6 +77,18 @@ function Ship(draft, crew) {
   this.crew = crew;
 }
 
+// Ahoy matey!
+// You are a leader of a small pirate crew. And you have a plan. With the help of OOP you wish to make a pretty efficient system to identify ships with a heavy booty on board.
+// Unfortunately for you, people weigh a lot these days, so how do you know if a ship is full of gold and not people?
+// You begin with writing a generic Ship class / struct:
+// function Ship(draft,crew) {
+//  this.draft = draft;
+//  this.crew = crew;
+// }
+// Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+// draft - an estimate of the ship's weight based on how low it is in the water
+// crew - the count of crew on board
+
 Ship.prototype.isWorthIt = function () {
   return this.draft - this.crew * 1.5 > 20;
 };
