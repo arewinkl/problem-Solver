b10d6ee7a1b5cc1eb7aedffad9eb2f087cca969a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-24 16:27:18 -0600

    changes to code

--- pageFourteenProblems.js
+++ pageFourteenProblems.js
@@ -92,3 +92,11 @@ function Ship(draft, crew) {
 Ship.prototype.isWorthIt = function () {
   return this.draft - this.crew * 1.5 > 20;
 };
+
+function Ship(draft, crew) {
+  this.draft = draft;
+  this.crew = crew;
+  this.isWorthIt = function () {
+    return this.draft - this.crew * 1.5 > 20;
+  };
+}
