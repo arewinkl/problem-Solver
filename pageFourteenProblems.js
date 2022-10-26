bb4935d1f6d56bb756a521c6c2a57fec7fcb3ee1
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-25 20:25:01 -0600

    instructions

--- pageFourteenProblems.js
+++ pageFourteenProblems.js
@@ -149,6 +149,10 @@ function reverse(array) {
   }, []);
 }
 
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
 function check(a, x) {
   let y = 0;
   for (let i = 0; i < a.length; i++) {
