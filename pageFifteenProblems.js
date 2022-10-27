73e2ebbc1eb1de0d31510c0db2642288982389f2
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-27 15:44:53 -0600

    kata cat

--- pageFifteenProblems.js
+++ pageFifteenProblems.js
@@ -31,5 +31,31 @@ const squares = (x, n) =>
         .fill(x)
         .map((m, i) => Math.pow(m, Math.pow(2, i)));
 
+// Description
+// An infinite number of shelves are arranged one above the other in a staggered fashion.
+// The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+//                  ┌────────┐
+//                  │-6------│
+//                  └────────┘
+// ┌────────┐
+// │------5-│
+// └────────┘  ┌─────► OK!
+//             │    ┌────────┐
+//             │    │-4------│
+//             │    └────────┘
+// ┌────────┐  │
+// │------3-│  │
+// BANG!────┘  ├─────► OK!
+//   ▲  |\_/|  │    ┌────────┐
+//   │ ("^-^)  │    │-2------│
+//   │ )   (   │    └────────┘
+// ┌─┴─┴───┴┬──┘
+// │------1-│
+// └────────┘
+// Input
+// Start and finish shelf numbers (always positive integers, finish no smaller than start)
+// Task
+// Find the minimum number of jumps to go from start to finish
+
 const solution = (start, finish, difference = finish - start) =>
   Math.floor(difference / 3) + (difference % 3);
