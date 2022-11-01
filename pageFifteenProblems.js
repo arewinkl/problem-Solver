e9ac560bc3b8caed5e577e9542b9f58cd837fb79
Alec Rewinkel <alecrewinkel@gmail.com>
2022-10-31 20:03:06 -0600

    code

--- pageFifteenProblems.js
+++ pageFifteenProblems.js
@@ -115,4 +115,10 @@ function validBraces(braces) {
   return depth.length == 0;
 }
 
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
 const getSumOfDigits = (int) => [...(int + "")].reduce((a, b) => a + +b, 0);
