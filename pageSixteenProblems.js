925800a11f71469c23c27dd88ac060923b33d7f9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-03 14:30:17 -0600

    alt2 solution

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -30,3 +30,7 @@ function basicOp(operation, value1, value2) {
       return value1 / value2;
   }
 }
+
+function basicOp(operation, value1, value2) {
+  return eval(value1 + operation + value2);
+}
