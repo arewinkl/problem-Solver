c8e58abb6355256cc8716d95b77bead96a3d74b9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-03 14:27:09 -0600

    switch case

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -17,3 +17,16 @@ function basicOp(operation, value1, value2) {
     return value1 * value2;
   } else return value1 / value2;
 }
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+  }
+}
