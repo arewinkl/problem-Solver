fca500194cd56b0845c88332956bea2eff42eb5d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-06 15:29:53 -0700

    code

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -66,6 +66,7 @@ function disemvowel(str) {
   return newArray.join("");
 }
 
+//!This is a simple solution that uses regex to remove the vowels from the string.
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, "");
 }
