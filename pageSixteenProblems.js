965fef06a3945633b7a3aade7f042dd19b8b89f0
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-08 19:22:46 -0700

    code kata

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -80,3 +80,38 @@ function disemvowel(str) {
     .filter((letter) => !vowels.includes(letter.toLowerCase()))
     .join("");
 }
+
+function switchItUp(number) {
+  switch (number) {
+    case (number = 0):
+      return "Zero";
+      break;
+    case (number = 1):
+      return "One";
+      break;
+    case (number = 2):
+      return "Two";
+      break;
+    case (number = 3):
+      return "Three";
+      break;
+    case (number = 4):
+      return "Four";
+      break;
+    case (number = 5):
+      return "Five";
+      break;
+    case (number = 6):
+      return "Six";
+      break;
+    case (number = 7):
+      return "Seven";
+      break;
+    case (number = 8):
+      return "Eight";
+      break;
+    case (number = 9):
+      return "Nine";
+      break;
+  }
+}
