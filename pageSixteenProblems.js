4211080d5907f8b2bd0d7500d729864d44812103
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-15 08:07:03 -0700

    fizzybuzzy

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -224,3 +224,19 @@ function fizzbuzz(n) {
   }
   return newArray;
 }
+
+function fizzbuzz(n) {
+  var i = 1,
+    arr = [];
+  while (i <= n) {
+    var fizz = i % 3 == 0;
+    var buzz = i % 5 == 0;
+    if (fizz || buzz) {
+      arr.push((fizz ? "Fizz" : "") + (buzz ? "Buzz" : ""));
+    } else {
+      arr.push(i);
+    }
+    i++;
+  }
+  return arr;
+}
