53c36076ef612e8f8aff1a4e2a0294497ead8a8a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-14 21:22:13 -0700

    code kata

--- pageSixteenProblems.js
+++ pageSixteenProblems.js
@@ -182,3 +182,12 @@ function spoonerize(words) {
 
   return [a, b].join(" ");
 }
+
+function numberJoy(n) {
+  var s = n
+    .toString()
+    .split("")
+    .reduce((s, v) => s + +v, 0);
+  var sr = +s.toString().split("").reverse().join("");
+  return s * sr == n;
+}
