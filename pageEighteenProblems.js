b67056acf97ea5ae012ab59314bb4ab873dd6eb9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-16 17:16:53 -0700

    alt solution

--- pageEighteenProblems.js
+++ pageEighteenProblems.js
@@ -52,3 +52,12 @@ function humanReadable(seconds) {
 
   return h + ":" + m + ":" + s;
 }
+
+function humanReadable(seconds) {
+  return [seconds / 3600, (seconds % 3600) / 60, seconds % 60]
+    .map(function (v) {
+      v = Math.floor(v).toString();
+      return v.length == 1 ? "0" + v : v;
+    })
+    .join(":");
+}
