0b99464fbf6d9b0d6bf69c65df5720a461465e68
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-18 16:00:17 -0700

    hotdogs

--- pageEighteenProblems.js
+++ pageEighteenProblems.js
@@ -65,3 +65,13 @@ function humanReadable(seconds) {
 
 //new problem
 //7 katas and harder
+
+function saleHotdogs(n) {
+  if (n < 5) {
+    return n * 100;
+  } else if (n >= 5 && n < 10) {
+    return n * 95;
+  } else {
+    return n * 90;
+  }
+}
