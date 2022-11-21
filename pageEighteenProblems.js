e8199a38b95248aa6ba0c892842c0117600c0ec2
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-20 19:02:31 -0700

    code

--- pageEighteenProblems.js
+++ pageEighteenProblems.js
@@ -88,3 +88,13 @@ function saleHotdogs(n) {
 function saleHotdogs(n) {
   return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
 }
+
+
+Positive integers that are divisible exactly by the sum of their digits are called Harshad numbers. The first few Harshad numbers are: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 18, ...
+
+We are interested in Harshad numbers where the product of its digit sum s and s with the digits reversed, gives the original number n. For example consider number 1729:
+
+its digit sum, s = 1 + 7 + 2 + 9 = 19
+reversing s = 91
+and 19 * 91 = 1729 --> the number that we started with.
+Complete the function which tests if a positive integer n is Harshad number, and returns True if the product of its digit sum and its digit sum reversed equals n; otherwise return False.
