f79a9835d8a361136f0362d0672fa699df794772
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-22 21:21:14 -0700

    code

--- pageEighteenProblems.js
+++ pageEighteenProblems.js
@@ -121,3 +121,15 @@ function numberJoy(n) {
 // input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
 // ouptut: 5
 // The most frequent number in the array is -1 and it occurs 5 times.
+
+function mostFrequentItemCount(collection) {
+  if (collection.length === 0) return 0;
+
+  var count = Object.create(null);
+
+  collection.forEach((item) => {
+    count[item] = (count[item] || 0) + 1;
+  });
+
+  return Math.max(...Object.values(count));
+}
