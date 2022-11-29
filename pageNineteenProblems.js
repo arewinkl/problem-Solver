c44024b894d28f94d9ab9847a04bbe31ec824e0c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-28 18:01:14 -0700

    cjane

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -1,3 +1,7 @@
+// DESCRIPTION:
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
 // Create a function called args_count that returns the number of arguments provided
 function args_count() {
   console.log(arguments.length);
