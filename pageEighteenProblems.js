b7b7778836b79c0d3ec597bd68f8d0161514456e
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-28 17:50:04 -0700

    added inst

--- pageEighteenProblems.js
+++ pageEighteenProblems.js
@@ -141,6 +141,8 @@ function mostFrequentItemCount(c) {
     : 0;
 }
 
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+
 function none(arr, fun) {
   console.log(arr.some(fun));
   return !arr.some(fun);
