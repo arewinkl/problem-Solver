7a22f431b93f3aae0259f789cb7fcd75c3723496
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-28 18:01:49 -0700

    change to comments

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -2,7 +2,7 @@
 // args_count(1, 2, 3) -> 3
 // args_count(1, 2, 3, 10) -> 4
 
-// Create a function called args_count that returns the number of arguments provided
+//! Create a function called args_count that returns the number of arguments provided
 function args_count() {
   console.log(arguments.length);
   return arguments.length;
