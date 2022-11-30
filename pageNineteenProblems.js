97d383cb5b141197cc80ff405155851cbe626399
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-29 17:53:30 -0700

    code chagne inst

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -8,10 +8,10 @@ function args_count() {
   return arguments.length;
 }
 
-// Puzzle
-// If you don't know who Waldo is, he's a nice guy who likes to be in crowded places. But he's also a bit odd as he always likes to hide in plain sight. Can you spot Waldo in the crowd?
-// Task
-// Given crowd, an array of strings of equal length, representing a crowded place, return an array with two integers representing the coordinates [y, x] where Waldo can be found ([0, 0] is top-left, y being the row and x being the column ).
+//! Puzzle
+//! If you don't know who Waldo is, he's a nice guy who likes to be in crowded places. But he's also a bit odd as he always likes to hide in plain sight. Can you spot Waldo in the crowd?
+//! Task
+//! Given crowd, an array of strings of equal length, representing a crowded place, return an array with two integers representing the coordinates [y, x] where Waldo can be found ([0, 0] is top-left, y being the row and x being the column ).
 // Examples
 // We've spotted Waldo a couple of times in the past. Below is what we found out. Note that he's been in much more crowded places lately.
 // #1 Waldo at the beach
