abc8593102963d36b4b3cccc5f3e3245258d021d
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-29 17:52:53 -0700

    code chagne

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -10,13 +10,10 @@ function args_count() {
 
 // Puzzle
 // If you don't know who Waldo is, he's a nice guy who likes to be in crowded places. But he's also a bit odd as he always likes to hide in plain sight. Can you spot Waldo in the crowd?
-
 // Task
 // Given crowd, an array of strings of equal length, representing a crowded place, return an array with two integers representing the coordinates [y, x] where Waldo can be found ([0, 0] is top-left, y being the row and x being the column ).
-
 // Examples
 // We've spotted Waldo a couple of times in the past. Below is what we found out. Note that he's been in much more crowded places lately.
-
 // #1 Waldo at the beach
 // "             "          Air
 // "         w   "           Air with a bird
@@ -26,9 +23,7 @@ function args_count() {
 // "...P......P.."           Beach with some people
 // "......P..P..."           Beach with some people
 // "..PW........."           Beach with Waldo and presumably a friend next to him
-
 // Unredacted report: Waldo can be found at [7, 3], wearing his usual outfit
-
 // #2 Waldo visiting the Great Pyramid
 // "                              "          Air
 // "                              "           Air
@@ -36,7 +31,6 @@ function args_count() {
 // "          _____               "           Layer of pyramid
 // "        _________             "           Layer of pyramid
 // "  B  _______________   G   GG "           Ground layer of pyramid with several people, including Waldo
-
 // Unredacted report: Waldo can be found at [5, 2], wearing special clothes protecting him from the sun
 
 function findWaldo(crowd) {
