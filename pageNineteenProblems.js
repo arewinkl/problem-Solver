745a93f4278c1369cc61d3677934884f6a1a41d9
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-29 18:08:33 -0700

    code

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -49,6 +49,8 @@ function findWaldo(crowd) {
   }
 }
 
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
 function mystery() {
   var results = { sanity: "Hello" };
   console.log(results.sanity);
