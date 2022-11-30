cca793af4d08e1a737c6e54a1eabe0f0d4c684c3
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-29 17:51:41 -0700

    kata wata

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -7,3 +7,19 @@ function args_count() {
   console.log(arguments.length);
   return arguments.length;
 }
+
+function findWaldo(crowd) {
+  let count = new Map();
+  for (let y = 0; y < crowd.length; y++) {
+    for (let x = 0; x < crowd[y].length; x++) {
+      count.set(crowd[y][x], (count.get(crowd[y][x]) || 0) + 1);
+    }
+  }
+  for (let y = 0; y < crowd.length; y++) {
+    for (let x = 0; x < crowd[y].length; x++) {
+      if (count.get(crowd[y][x]) == 1) {
+        return [y, x];
+      }
+    }
+  }
+}
