97319d52c21f4a9d4de1eff553b4418bd11b1fff
Alec Rewinkel <alecrewinkel@gmail.com>
2022-12-20 07:50:18 -0700

    code kata alt

--- pageNineteenProblems.js
+++ pageNineteenProblems.js
@@ -97,3 +97,9 @@ function hoopCount(n) {
   if (n < 10) return "Keep at it until you get it";
   if (n > 9) return "Great, now move on to tricks";
 }
+
+function hoopCount(n) {
+  return n < 10
+    ? "Keep at it until you get it"
+    : "Great, now move on to tricks";
+}
